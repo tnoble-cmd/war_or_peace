4b7a4371991262aaa030361666679fdf12f6175c
Tyler Noble <tmnoble11@gmail.com>
2024-05-12 22:26:46 -0600

    Added war? and mutually_assured_destruction? methods. Added 3 turn.type tests to check types of turns for each method basic, war, mutually_assured_destrucion?

--- lib/turn.rb
+++ lib/turn.rb
@@ -14,16 +14,26 @@ class Turn
     def type
         if basic?
             :basic
+        elsif mutually_assured_destruction?
+            :mutually_assured_destruction?
         elsif war?
             :war
-        elsif mutually_assured_destruction
-            :mutually_assured_destruction
         end
     end
 
     def basic?
         player_1.stack.rank_of_cards_at(0) != player_2.stack.rank_of_cards_at(0)
     end
+
+    def war?
+        player_1.stack.rank_of_cards_at(0) == player_2.stack.rank_of_cards_at(0)
+    end
+        #face up is equal, put one card face down and another face up, if both equal NO WIN. No spoil of war
+    def mutually_assured_destruction?
+        player_1.stack.rank_of_cards_at(0) == player_2.stack.rank_of_cards_at(0) &&
+        player_1.stack.rank_of_cards_at(2) == player_2.stack.rank_of_cards_at(2)
+    end
+
 
 
 end
--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -6,13 +6,13 @@ require './lib/card'
 
 RSpec.describe Turn do
     
-    before(:each) do
+    it 'exists' do
         @card1 = Card.new(:diamond, 'Queen', 12)
         @card2 = Card.new(:spade, '3', 3)
         @card3 = Card.new(:heart, 'Ace', 14)
-        @card4 = Card.new(:heart, 'King', 13)
+        @card4 = Card.new(:heart, 'King', 12)
         @card5 = Card.new(:spade, '10', 10)
-        @card6 = Card.new(:heart, '2', 2)
+        @card6 = Card.new(:heart, '2', 14)
 
         @deck1 = Deck.new([@card1, @card2, @card3])
         @deck2 = Deck.new([@card4, @card5, @card6])
@@ -20,15 +20,70 @@ RSpec.describe Turn do
         @player1 = Player.new('Clarisa', @deck1)
         @player2 = Player.new('John', @deck2)
         @turn = Turn.new(@player1, @player2)
-    end
-    it 'exists' do
 
         expect(@turn).to be_an_instance_of(Turn)
     end
 
-    it 'checks turn type' do
+    it 'checks turn type :basic' do
+
+        @card1 = Card.new(:diamond, 'Queen', 12)
+        @card2 = Card.new(:spade, '3', 3)
+        @card3 = Card.new(:heart, 'Ace', 14)
+        @card4 = Card.new(:heart, 'King', 13)
+        @card5 = Card.new(:spade, '10', 10)
+        @card6 = Card.new(:heart, '2', 14)
+
+        @deck1 = Deck.new([@card1, @card2, @card3])
+        @deck2 = Deck.new([@card4, @card5, @card6])
+
+        @player1 = Player.new('Clarisa', @deck1)
+        @player2 = Player.new('John', @deck2)
+        @turn = Turn.new(@player1, @player2)
 
         expect(@turn.type).to eq(:basic)
+    end
+
+    it 'checks turn type :war' do
+
+        @card1 = Card.new(:heart, 'Jack', 11)
+        @card2 = Card.new(:heart, '10', 10)
+        @card3 = Card.new(:heart, '9', 9)
+        @card4 = Card.new(:diamond, 'Jack', 11)
+        @card5 = Card.new(:heart, '8', 8)
+        @card6 = Card.new(:diamond, 'Queen', 12)
+        @card7 = Card.new(:heart, '3', 3)
+        @card8 = Card.new(:diamond, '2', 2)
+
+        @deck1 = Deck.new([@card1, @card2, @card5, @card8])
+        @deck2 = Deck.new([@card4, @card3, @card6, @card7])
+
+        @player1 = Player.new("Megan", @deck1)
+        @player2 = Player.new("Aurora", @deck2)
+
+        @turn = Turn.new(@player1, @player2)
+
+        expect(@turn.type).to eq(:war)
+    end
+
+    it 'checks turn type :mutually_assured_destruction' do
+        @card1 = Card.new(:heart, 'Jack', 11)
+        @card2 = Card.new(:heart, '10', 10)
+        @card3 = Card.new(:heart, '9', 9)
+        @card4 = Card.new(:diamond, 'Jack', 11)
+        @card5 = Card.new(:heart, '8', 8)
+        @card6 = Card.new(:diamond, '8', 8)
+        @card7 = Card.new(:heart, '3', 3)
+        @card8 = Card.new(:diamond, '2', 2)
+
+        @deck1 = Deck.new([@card1, @card2, @card5, @card8])
+        @deck2 = Deck.new([@card4, @card3, @card6, @card7])
+
+        @player1 = Player.new("Megan", @deck1)
+        @player2 = Player.new("Aurora", @deck2)
+
+        @turn = Turn.new(@player1, @player2)
+
+        expect(@turn.type).to eq(:mutually_assured_destruction?)
     end
 
 
